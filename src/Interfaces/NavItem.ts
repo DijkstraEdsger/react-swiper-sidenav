1ae056bb535ec3b83cdfbd1f7116eb4369444635
Humberto <humbertomartinez900519@gmail.com>
2023-11-09 11:25:10 +0300

    enhancing code

--- src/Interfaces/NavItem.ts
+++ src/Interfaces/NavItem.ts
@@ -9,3 +9,25 @@ export interface INavItemProps {
   disableClose: boolean
   itemsClassName: string
 }
+
+export interface IIconLinkItemProps {
+  disableClose: boolean
+  clickedLink: () => void
+  children: React.JSX.Element | string
+  itemsClassName: string
+  itemProps: React.JSX.ElementAttributesProperty
+}
+
+export interface IRenderItemProps {
+  disableClose: boolean
+  clicked: () => void
+  children: React.JSX.Element | string
+}
+
+export interface ISliceItemProps {
+  clicked: () => void
+  children: React.JSX.Element | string
+  itemsClassName: string
+  className: string
+  itemProps: React.JSX.ElementAttributesProperty
+}
