33e5c8598920a1929709d0fc6909053880c83ac1
Humberto <humbertomartinez900519@gmail.com>
2023-11-10 13:41:11 +0300

    refactoring

--- src/components/SideDrawer/useSideDrawer.tsx
+++ src/components/SideDrawer/useSideDrawer.tsx
@@ -1,48 +1,51 @@
-import { CSSProperties, useEffect, useMemo, useState } from 'react'
+import { Placement, Variant } from 'Interfaces/SideDrawer'
+import { useEffect, useMemo, useState } from 'react'
 
-const useSideDrawer = ({ navItems, variant, placement, open }: any) => {
-  const [positions, setPositions] = useState(new Array<string>())
+type SideDrawerProps = {
+  navItems: any[]
+  variant: Variant
+  placement: Placement
+  open: boolean
+}
+
+type SideDrawerReturn = {
+  positions: string[]
+  className: string
+  cssPosition: React.CSSProperties['position']
+  slideForward: (parent: number, child: number) => void
+  slideBackward: (parent: number, current: number) => void
+}
+
+const useSideDrawer = ({ navItems, variant, placement, open }: SideDrawerProps): SideDrawerReturn => {
+  const [positions, setPositions] = useState<string[]>([])
 
   useEffect(() => {
     if (navItems) {
-      const initialPositions = navItems.map((_: any, index: number) => (index === 0 ? '0' : '100%'))
+      const initialPositions = navItems.map((_, index) => (index === 0 ? '0' : '100%'))
       setPositions(initialPositions)
     }
   }, [navItems])
 
-  const slideForward = (parent: number, child: number) => {
+  const slide = (parent: number, child: number, parentPos: string, childPos: string) => {
     const m = [...positions]
-    m[parent] = '-100%'
-    m[child] = '0'
+    m[parent] = parentPos
+    m[child] = childPos
     setPositions(m)
   }
 
-  const slideBackward = (parent: number, current: number) => {
-    const m = [...positions]
-    m[parent] = '0'
-    m[current] = '100%'
-    setPositions(m)
-  }
+  const slideForward = (parent: number, child: number) => slide(parent, child, '-100%', '0')
+  const slideBackward = (parent: number, current: number) => slide(parent, current, '0', '100%')
 
   const className = useMemo(() => {
-    let classesSideDrawer = ['SideDrawer', 'SideDrawerLeft', 'CloseLeft']
-
-    if (placement === 'right') {
-      classesSideDrawer = ['SideDrawer', 'SideDrawerRight', 'CloseRight']
-    }
-
-    if (open) {
-      classesSideDrawer = ['SideDrawer', 'SideDrawerLeft', 'Open']
-
-      if (placement === 'right') {
-        classesSideDrawer = ['SideDrawer', 'SideDrawerRight', 'Open']
-      }
-    }
-
+    const classesSideDrawer = [
+      'SideDrawer',
+      `SideDrawer${placement.charAt(0).toUpperCase() + placement.slice(1)}`,
+      open ? 'Open' : `Close${placement.charAt(0).toUpperCase() + placement.slice(1)}`,
+    ]
     return classesSideDrawer.join(' ')
   }, [open, placement])
 
-  const cssPosition = useMemo<CSSProperties['position']>(
+  const cssPosition = useMemo<React.CSSProperties['position']>(
     () => (variant === 'permanent' ? 'inherit' : 'fixed'),
     [variant],
   )
