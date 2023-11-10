2d14ccb359aab5ba10f86ccaca7596958254b1b0
Humberto <humbertomartinez900519@gmail.com>
2023-11-10 13:44:40 +0300

    refactoring

--- src/components/SideNav/useSideNav.tsx
+++ src/components/SideNav/useSideNav.tsx
@@ -1,7 +1,22 @@
+import { Variant } from 'Interfaces/SideDrawer'
 import { useMemo } from 'react'
 import { convertMenuTreeToArray } from 'utils/functions'
 
-const useSideNav = ({ navItems, variant, hideBackdrop, open }: any) => {
+type SideNavProps = {
+  navItems: any[]
+  variant: Variant
+  hideBackdrop: boolean
+  open: boolean
+}
+
+type SideNavReturn = {
+  preProcessedNavItems: any[]
+  className: string
+  showBackdrop: boolean
+  openSideDrawer: boolean
+}
+
+const useSideNav = ({ navItems, variant, hideBackdrop, open }: SideNavProps): SideNavReturn => {
   const preProcessedNavItems = useMemo(() => (navItems ? convertMenuTreeToArray(navItems) : []), [navItems])
   const className = useMemo(() => (variant === 'permanent' ? 'permanentDisplay' : 'fixedDisplay'), [variant])
   const showBackdrop = useMemo(() => !hideBackdrop && variant !== 'permanent', [hideBackdrop, variant])
